74799fe860412d0792d37e84e1497108e6bb9e85
Kadłubek <179640@edu.p.lodz.pl>
2016-10-24 21:25:11 +0200

    Added CallbackInjectable interface to the Main class and created implementation stubs

--- src/main/java/pl/lodz/p/zad2cli/ind179640ind187824/Main.java
+++ src/main/java/pl/lodz/p/zad2cli/ind179640ind187824/Main.java
@@ -1,12 +1,18 @@
 package pl.lodz.p.zad2cli.ind179640ind187824;
 
+import pl.lodz.p.zad2.ind179640ind187824.CallbackInjectable;
 import pl.lodz.p.zad2.ind179640ind187824.QuestionCallback;
 
-public class Main {
+public class Main implements CallbackInjectable{
 	
 	private QuestionCallback callback;
 	
 	public static void main(String... args){
+		
+	}
+
+	public void setCallback(QuestionCallback arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 }
