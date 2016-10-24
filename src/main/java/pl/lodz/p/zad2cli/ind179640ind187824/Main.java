f4906468e52ff2776b463d6a5a874618277d19b4
Kadłubek <179640@edu.p.lodz.pl>
2016-10-24 21:23:51 +0200

    added callback field

--- src/main/java/pl/lodz/p/zad2cli/ind179640ind187824/Main.java
+++ src/main/java/pl/lodz/p/zad2cli/ind179640ind187824/Main.java
@@ -1,6 +1,10 @@
 package pl.lodz.p.zad2cli.ind179640ind187824;
 
+import pl.lodz.p.zad2.ind179640ind187824.QuestionCallback;
+
 public class Main {
+	
+	private QuestionCallback callback;
 	
 	public static void main(String... args){
 		
