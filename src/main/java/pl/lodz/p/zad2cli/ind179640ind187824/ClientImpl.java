4f9f26cf108e0941b356eb29647ce38ec266f768
Kadłubek <179640@edu.p.lodz.pl>
2016-11-07 22:02:33 +0100

    Added window listener

--- src/main/java/pl/lodz/p/zad2cli/ind179640ind187824/ClientImpl.java
+++ src/main/java/pl/lodz/p/zad2cli/ind179640ind187824/ClientImpl.java
@@ -1,21 +1,31 @@
 package pl.lodz.p.zad2cli.ind179640ind187824;
 
 import java.awt.GridLayout;
-import java.util.Scanner;
+import java.awt.Window;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 import pl.lodz.p.zad2.ind179640ind187824.QuestionCallback;
+import pl.lodz.p.zad2.ind179640ind187824.Root;
 
 public class ClientImpl implements Client {
 	
     private QuestionCallback callback;
 	
 	private JFrame inputDialog;
+
+	private JTextField fileNameText;
+
+	private JTextField correctText;
+
+	private JTextField wrongText;
 	
 	public void showDialog(){
 		init();
@@ -27,13 +37,13 @@ public class ClientImpl implements Client {
 		inputDialog = new JFrame();
 
 		JLabel fileNameLabel = new JLabel("File name:");
-		JTextField fileNameText = new JTextField();
+		fileNameText = new JTextField();
 		
 		JLabel correctTextLabel = new JLabel("Correct text:");
-	    JTextField correctText = new JTextField();
+	    correctText = new JTextField();
 	    
 	    JLabel wrongTextLabel = new JLabel("Wrong text:");
-	    JTextField wrongText = new JTextField();
+	    wrongText = new JTextField();
 	    
 
 
@@ -52,6 +62,10 @@ public class ClientImpl implements Client {
 	    
 	    inputDialog.setContentPane(panel);
 	    
+	    inputDialog.setSize(200, 250);
+	    
+	    inputDialog.setResizable(false);
+	    
 	    inputDialog.setVisible(true);
 		
 	}
@@ -61,9 +75,41 @@ public class ClientImpl implements Client {
 	public void setCallback(QuestionCallback callback) {
 		
 		this.callback = callback;
+		
+		inputDialog.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		
+		Window window = SwingUtilities.windowForComponent(inputDialog);
+		
+		WindowListener windowListener = createWindowClosingListener();
+		
+		window.addWindowListener(windowListener);
 	}
 
 	
+	private WindowListener createWindowClosingListener() {
+		
+		WindowListener windowListener = new WindowAdapter(){
+			@Override
+			public void windowClosing(WindowEvent e) {
+				
+				boolean success = Root.saveFile(fileNameText.getText(), correctText.getText(), wrongText.getText());
+				
+				boolean close = callback.doRetry(success);
+				
+				if(close){
+					inputDialog.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				} 
+				
+				super.windowClosing(e);
+			}
+		};
+		
+		
+		return windowListener;
+	}
+
+
+
 	@Override
 	public boolean canApplicationBeClosed(boolean success){
 		
--- src/main/java/pl/lodz/p/zad2cli/ind179640ind187824/Main.java
+++ src/main/java/pl/lodz/p/zad2cli/ind179640ind187824/Main.java
@@ -1,12 +1,5 @@
 package pl.lodz.p.zad2cli.ind179640ind187824;
 
-import java.util.Scanner;
-
-import javax.swing.JFrame;
-
-import pl.lodz.p.zad2.ind179640ind187824.QuestionCallback;
-import pl.lodz.p.zad2.ind179640ind187824.Root;
-
 public class Main {
 	
 
@@ -15,7 +8,7 @@ public class Main {
 		
 		Client client = new ClientImpl();
 
-		
+		client.showDialog();
 		
 	}
 
