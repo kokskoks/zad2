15a49aa043009671c774ef7979d13db35b22d4ac
Kadłubek <179640@edu.p.lodz.pl>
2016-11-07 21:21:33 +0100

    Two new method stubs

--- src/main/java/pl/lodz/p/zad2cli/ind179640ind187824/ClientImpl.java
+++ src/main/java/pl/lodz/p/zad2cli/ind179640ind187824/ClientImpl.java
@@ -1,5 +1,65 @@
 package pl.lodz.p.zad2cli.ind179640ind187824;
 
-public class ClientImpl {
+import java.util.Scanner;
 
+import javax.swing.JFrame;
+
+import pl.lodz.p.zad2.ind179640ind187824.QuestionCallback;
+import pl.lodz.p.zad2.ind179640ind187824.Root;
+
+public class ClientImpl implements Client {
+	
+    private QuestionCallback callback;
+	
+	private JFrame inputDialog;
+	
+	public void showDialog(){
+		init();
+	}
+	
+	
+
+	private void init() {
+		
+		
+	}
+
+
+
+	public void setCallback(QuestionCallback callback) {
+		
+		this.callback = callback;
+	}
+
+	@Override
+	public String[] getInfo() {
+		String[] data = new String[3];
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Podaj nazwe");
+		data[0] = scanner.nextLine();
+		System.out.println("Podaj poprawny string:");
+		data[1] = scanner.nextLine();
+		System.out.println("Podaj niepoprawny string:");
+		data[2] = scanner.nextLine();
+		return data;
+	}
+	
+	@Override
+	public boolean canApplicationBeClosed(boolean success){
+		
+		boolean closeApplication = false;
+		
+		if(callback!=null){
+			closeApplication = !callback.doRetry(success);
+		}
+		
+		return closeApplication;
+	}
+
+	@Override
+	public JFrame getJFrameInstance() {
+		
+		return inputDialog;
+	}
+	
 }
